20dcaa9d46e9c7216b36e21137cd50cc5ec7dee7
JusiLG <84391754+JusiLG@users.noreply.github.com>
2021-08-09 16:36:24 -0500

    listado

--- src/main/webapp/assets/dist/js/main.js
+++ src/main/webapp/assets/dist/js/main.js
@@ -1,7 +1,22 @@
-$('[id^="btn-delete"]').click( function() {
-    let code = $(this).attr('data-code');
-    let text = $(this).attr('data-text');
+$('[id^="content"]').click(function(){
+    let id = $(this).attr('data-code');
 
-    $('#id').val(code);
-    $('#text-delete').text(text);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/servletAsync_3C_war/findById',
+        data: {
+            action: 'findById',
+            id: id
+        }
+    }).done(function(res){
+        console.log(res);
+        let games = res.games;
+
+        $('#lbl_idGames').text(games.idGames);
+        $('#lbl_name').text(games.name);
+        $('#lbl_imgGame').text(games.imgGame);
+        $('#lbl_datePremiere').text(games.datePremiere);
+        $('#lbl_Category_idCategory').text(games.Category_idCategory);
+        $('#lbl_status').text(games.status);
+    });
 });
